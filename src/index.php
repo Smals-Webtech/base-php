23ee416bc73e17fd18e24a7b776f882c1404e7b0
Sébastian Molle <sebastian.molle@gmail.com>
2025-07-22 20:41:43 +0200

    chore(): Initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Default index page for Docker base image.
+ *
+ * This file is used as the default landing page when the container is started.
+ *
+ * PHP version 8.4
+ *
+ * @category Docker
+ * @package  DockerBaseImage
+ * @author   Sebastian Molle <sebastian.molle@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/smalswebtech/base-php
+ */
+
+declare(strict_types=1);
+
+echo 'Docker Base image - Default index.php page';
